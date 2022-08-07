d688c3eb9499f307c4a6c74dc87402c23376dec6
unknown <buragohain4@gmail.com>
2022-08-07 11:00:00 +0530

    Implemented UC1

---
+++ UC1/UC1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace UC1
+{
+    class ContactBook
+    {
+        static void Main(string[] args)
+        {
+            ContactBook createContacts = new ContactBook();
+            createContacts.AddYourContact();
+        }
+        public void AddYourContact()
+        {
+            string contact;
+            Console.WriteLine("Please Enter Contact Details: ");
+            Console.Write("First Name : ");
+            String FirstName = Console.ReadLine();
+            Console.Write("Last Name : ");
+            String LastName = Console.ReadLine();
+            Console.Write("Phone Number : ");
+            long Phone_Number = Convert.ToInt64(Console.ReadLine());
+            Console.Write("Email : ");
+            String Email = Console.ReadLine(); ;
+            Console.Write("Address : ");
+            String Address = Console.ReadLine();
+            Console.Write("City : ");
+            String City = Console.ReadLine();
+            Console.Write("State : ");
+            String State = Console.ReadLine();
+            Console.Write("Pincode : ");
+            int PinCode = Convert.ToInt32(Console.ReadLine());
+            contact = FirstName + " " + LastName + " " + Phone_Number.ToString() + " " + Email + " " + Address + " " + City + " " + State + " " + PinCode.ToString();
+           
+            Console.WriteLine("Thanks! Your contact details are saved in our Book.");
+            Console.WriteLine(contact);
+        }
+    }
+}
